bd7da360334494e8137c8c150783bf24d301fa0c
KM5075 <koji19991999@gmail.com>
2025-05-18 18:28:20 +0900

    Feat: 新規テストプロジェクトとテストクラスの追加
    新しいテストプロジェクト `Plated_Blazor.Client.Tests` をソリューションに追加しました。このプロジェクトには以下の変更が含まれます:
    - `AddRecipe` コンポーネントのテストクラス `AddRecipeTests` を追加。
      - `RedirectToHome_Test` と `AddRecipePage_Test` メソッドを実装。
      - テスト用に `FakeNavigationManager` クラスを作成。
    - `Plated_Blazor.Client.Tests.csproj` を作成し、必要な依存関係を追加。

---
+++ Plated_Blazor/Plated_Blazor.Client.Tests/AddRecipeTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Plated_Blazor.Client.Components.Pages;
+using Plated_Blazor.Client.Tests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plated_Blazor.Client.Components.Pages.Tests
+{
+    [TestClass()]
+    public class AddRecipeTests
+    {
+        [TestMethod()]
+        public void RedirectToHome_Test()
+        {
+            // Arrange
+            var addRecipe = new AddRecipe();
+            var fakeNavigationManager = new FakeNavigationManager();
+            addRecipe.NavigationManager = fakeNavigationManager;
+
+            // Act
+            addRecipe.RedirectToHome();
+
+            // Assert
+            Assert.AreEqual("/", fakeNavigationManager.NavigatedTo);
+        }
+
+        [TestMethod()]
+        public void AddRecipePage_Test()
+        {
+            // Arrange
+            var addRecipe = new AddRecipe();
+            var fakeNavigationManager = new FakeNavigationManager();
+            addRecipe.NavigationManager = fakeNavigationManager;
+            // Act
+            addRecipe.AddRecipePage();
+            // Assert
+            Assert.AreEqual("/", fakeNavigationManager.NavigatedTo);
+        }
+    }
+}
